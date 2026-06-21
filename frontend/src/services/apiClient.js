9011b121e6386d4db614de3c7ca7407187a1e167
FraPier99 <francesco.pierucci@icloud.com>
2026-06-22 01:29:42 +0200

    fix cors

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000'
 const AUTH_TOKEN_KEY = 'kart_auth_token'
 
+
+console.log('API_BASE_URL:', API_BASE_URL)
+
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
     headers: {
